7aefae4c4e376b17f63c4a4c9c010b536ec80549
Aki Laki <akilaki071@gmail.com>
2018-06-28 00:06:10 +0600

    One more rep

---
+++ www/header.php
@@ -0,0 +1,35 @@
+<header id="fh5co-header-section">
+				<div class="container-fluid">
+					<div class="nav-header">
+						<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
+						<ul class="new_ul">
+						<li><a href="#">+ 996 772 303 241</a></li>
+						<li><a href="#">+ 996 772 303 241</a></li>
+						<li><a href="#">+ 996 772 303 241</a></li>
+						
+						</ul>
+						<a href="#" class="loop">akilaki071@gmail.com</a>
+						<button class="btn_1">Заказать звонок</button>
+						
+						
+						<p class="loup">
+						
+								<a href="#"><i class="icon-twitter2"></i></a>
+								<a href="#"><i class="icon-facebook2"></i></a>
+								<a href="#"><i class="icon-instagram"></i></a>
+								<a href="#"><i class="icon-dribbble2"></i></a>
+								<a href="#"><i class="icon-youtube"></i></a>
+						
+						</p>
+
+					</div>
+				</div>
+				<nav id="own">
+				<a href="#" class="rotues">Главная</a>
+				<a href="#" class="rotues">Туры</a>
+				<a href="#" class="rotues">Визы</a>
+				<a href="#" class="rotues">Авиабилеты</a>
+				<a href="#" class="rotues">Контакты</a>
+				<a href="#" class="rotues">Акции</a>
+				</nav>
+			</header>
---
+++ www/states/amsterdam.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Главная страничка</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="favicon.ico">
+
+	<!-- <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css'> -->
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Superfish -->
+	<link rel="stylesheet" href="css/superfish.css">
+
+	<link rel="stylesheet" href="css/style.css">
+
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	</head>
+	<body>
+	<div id="fh5co-header">
+	<?php require_once("header.php");?>
+	</div>
+	<div id="wrap_1">
+	<div class="block_12">
+	<img src="images/img-2.jpg">
+	</div>
+	<form>
+	
+	</form>
+	</div>
+	</body>
+	</html>
